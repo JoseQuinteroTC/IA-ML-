0b643b2357c8a43ff300aad2ce9f8b36c9c05bf7
JoseQuinteroTC <67284004+JoseQuinteroTC@users.noreply.github.com>
2023-10-27 13:21:24 -0500

    Tratamiento de los datos

--- IA ML/datos.py
+++ IA ML/datos.py
@@ -129,7 +129,7 @@ for categoria, palabras in palabras_por_categoria.items():
     wordclouds[categoria] = wordcloud
 
 # Display the word clouds for each category
-plt.figure(figsize=(12, 6))
+plt.figure(figsize=(12, 10))
 for i, (categoria, wordcloud) in enumerate(wordclouds.items()):
     plt.subplot(1, 2, i+1)
     plt.imshow(wordcloud, interpolation='bilinear')
@@ -139,48 +139,380 @@ plt.tight_layout()
 plt.show()
 
 
-"""
-#Cargando datos
-diabetes = pd.read_excel("./datos_excel.xlsx")
-#Informacion de los datos
+#@title Recuento total de palabras por categoría
+word_count = texto_tidy.groupby('idioma')['token'].count()
 
+# Crear la tabla gráfica
+fig, ax = plt.subplots(figsize=(6, 4))
+colors = ['red', 'green']  # Colores para las categorías 'rotten' y 'fresh'
+word_count.plot(kind='bar', ax=ax, color=colors)
+ax.set_xlabel('Categoría')
+ax.set_ylabel('Recuento de palabras')
+ax.set_title('Recuento total de palabras por categoría')
 
-print("\n===================================\n Resumen de los datos \n")
-print(diabetes.info(),"\n===================================")
-
-print("\n===================================\n Cantidad de datos nulos por cada columna \n")
-print(diabetes.isnull().sum(),"\n===================================")
-
-print("\n===================================\n Resumen estadistico \n")
-print(diabetes.describe(),"\n===================================")
-
-
-
-#Histograma del atributo clase
-ax=plt.subplots(1,1,figsize=(15,15))
-sns.countplot(x='idioma',data=diabetes)
-plt.title("Outcome Count")
+# Mostrar la tabla gráfica
 plt.show()
 
-X_train, X_test, y_train, y_test = train_test_split(diabetes['texto'], diabetes['idioma'], train_size=0.2)
+print('--------------------------')
+print('Palabras totales por categoría 👇')
+print('--------------------------')
+texto_tidy.groupby(by='idioma')['token'].count()
+print(word_count)
 
-vectorizer = CountVectorizer()
-vector = vectorizer.fit_transform(X_train)
+# Palabras distintas utilizadas 
+# ==============================================================================
+print('----------------------------')
+print('Palabras distintas por categoria')
+print('----------------------------')
+print(texto_tidy.groupby(by='idioma')['token'].nunique())
 
-df = pd.DataFrame(vector, columns =['x'])
+
+#@title Gráfico porcentaje de palabras distintas por categoría
+unique_words = texto_tidy.groupby('idioma')['token'].nunique()
+
+# Definir los colores de las categorías
+colors = ['green', 'red']
+
+# Crear el gráfico de pastel
+fig, ax = plt.subplots()
+patches, texts, autotexts = ax.pie(unique_words, labels=unique_words.index, autopct='%1.1f%%', startangle=90, colors=colors)
+
+# Agregar los valores numéricos junto a las etiquetas
+for i, label in enumerate(texts):
+    percentage = label.get_text()
+    count = unique_words[i]
+    label.set_text(f'{percentage} ({count})')
+
+ax.set_title('Palabras distintas por categoría')
+
+# Mostrar el gráfico de pastel
+plt.show()
+
+
+
+# ==============================================================================
+palabras_en = set(texto_tidy[texto_tidy.idioma == 'en']['token'])
+palabras_nl = set(texto_tidy[texto_tidy.idioma == 'nl']['token'])
+
+
+print(f"Palabras comunes entre Ingles y Nerlandes: {len(palabras_en.intersection(palabras_nl))}")
+
+# Guardar en un csv las palabras que estan presentes en ambos idiomas
+palabras_comunes = palabras_en.intersection(palabras_nl)
+palabras_comunes_lista = list(palabras_comunes)
+
+df_palabras_comunes = pd.DataFrame(palabras_comunes_lista, columns=['Palabra'])
+df_palabras_comunes.to_csv('palabras_comunes.csv', index=False)
+
+
+#@title Nube las palabras comunes en las dos categorias
+# Convertir los elementos de palabras_comunes a cadenas de texto
+palabras_comunes_str = [str(palabra) for palabra in palabras_comunes]
+
+# Crear una cadena de texto con las palabras comunes
+texto_comun = ' '.join(palabras_comunes_str)
+
+# Crear el objeto WordCloud
+wordcloud = WordCloud(width=800, height=400, background_color='white').generate(texto_comun)
+
+# Mostrar la nube de palabras
+plt.figure(figsize=(9, 6))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')
+plt.title('Nube de palabras - Palabras comunes en las dos categorias')
+plt.show()
+
+
+# Cálculo del log of odds ratio de cada palabra (rotten vs fresh)
+# ==============================================================================
+# Pivotaje y despivotaje
+comentarios_pivot = texto_tidy.groupby(["idioma","token"])["token"] \
+                .agg(["count"]).reset_index() \
+                .pivot(index = "token" , columns="idioma", values= "count")
+
+comentarios_pivot = comentarios_pivot.fillna(value=0)
+comentarios_pivot.columns.name = None
+
+comentarios_unpivot = comentarios_pivot.melt(value_name='n', var_name='idioma', ignore_index=False)
+comentarios_unpivot = comentarios_unpivot.reset_index()
+
+# Selección de las categorias rotten y fresh
+comentarios_unpivot = comentarios_unpivot[comentarios_unpivot.idioma.isin(['en', 'nl'])]
+
+# Se añade el total de palabras de cada categoria
+comentarios_unpivot = comentarios_unpivot.merge(
+                    texto_tidy.groupby('idioma')['token'].count().rename('N'),
+                    how = 'left',
+                    on  = 'idioma'
+                 )
+
+# Cálculo de odds y log of odds de cada palabra
+comentarios_logOdds = comentarios_unpivot.copy()
+comentarios_logOdds['odds'] = (comentarios_logOdds.n + 1) / (comentarios_logOdds.N + 1)
+comentarios_logOdds = comentarios_logOdds[['token', 'idioma', 'odds']] \
+                    .pivot(index='token', columns='idioma', values='odds')
+comentarios_logOdds.columns.name = None
+
+comentarios_logOdds['log_odds']     = np.log(comentarios_logOdds.en/comentarios_logOdds.nl)
+comentarios_logOdds['abs_log_odds'] = np.abs(comentarios_logOdds.log_odds)
+
+# Si el logaritmo de odds es mayor que cero, significa que es una palabra con
+# mayor probabilidad de ser de rotten. Esto es así porque el ratio se ha
+# calculado como rotten/fresh.
+comentarios_logOdds['categoria frecuente'] = np.where(comentarios_logOdds.log_odds > 0,
+                                              "en",
+                                              "nl"
+                                    )
+
+print('-----------------------------------')
+print('Top 10 palabras más diferenciadoras')
+print('-----------------------------------')
+print(comentarios_logOdds.sort_values('abs_log_odds', ascending=False).head(15))
+
+
+#@title Top 15 palabras más características de cada categoria
+# ==============================================================================
+
+top_30 = comentarios_logOdds[['log_odds', 'abs_log_odds', 'categoria frecuente']] \
+        .groupby('categoria frecuente') \
+        .apply(lambda x: x.nlargest(15, columns='abs_log_odds').reset_index()) \
+        .reset_index(drop=True) \
+        .sort_values('log_odds')
+
+f, ax = plt.subplots(figsize=(4, 7))
+sns.barplot(
+    x='log_odds',
+    y='token',
+    hue='categoria frecuente',
+    data=top_30,
+    ax=ax,
+    palette={'en': 'red', 'nl': 'green'}  # Cambiar los colores aquí
+)
+ax.set_title('Top 15 palabras más características')
+ax.set_xlabel('log odds ratio (en / nl)')
+
+
+# Cálculo term-frecuency (tf)
+# ==============================================================================
+tf = texto_tidy.copy()
+# Número de veces que aparece cada término en cada comentario
+tf = tf.groupby(["token"])["token"].agg(["count"]).reset_index()
+# Se añade una columna con el total de términos por comentario
+tf['total_count'] = tf.groupby('token')['count'].transform(sum)
+# Se calcula el tf
+tf['tf'] = tf["count"] / tf["total_count"]
+print("=======================")
+print(tf.sort_values(by = "tf").head(5))
+
+# Inverse document frequency
+# ==============================================================================
+idf = texto_tidy.copy()
+total_documents = idf["token"].drop_duplicates().count()
+# Número de comentarios en los que aparece cada término
+idf = idf.groupby(["token"])["token"].agg(["count"]).reset_index()
+idf[' n_veces'] = idf.groupby('token')['count'].transform(sum)
+# Cálculo del idf
+idf['idf'] = np.log(total_documents / idf[' n_veces'])
+idf = idf[["token"," n_veces", "idf"]].drop_duplicates()
+print("=======================")
+print(idf.sort_values(by="idf").head(5))
+
+
+# Term Frequency - Inverse Document Frequency
+# ==============================================================================
+tf_idf = pd.merge(left=tf, right=idf, on="token")
+tf_idf["tf_idf"] = tf_idf["tf"] * tf_idf["idf"]
+print("=======================")
+print(tf_idf.sort_values(by="token").head())
+
+
+
+
+
+
+# Leer el archivo CSV
+df = pd.read_csv("./hola.csv")
+
+# Acceder a los datos de las columnas
+idioma = df['idioma']
+texto = df['texto']
+texto_tokenizado = df['texto_tokenizado']
+print("=======================")
 print(df.head())
-df.to_excel('x.xlsx')
-"""
 
 
+datos_X = df['texto_tokenizado']
+datos_y = df['idioma']
+print("=======================")
+print(datos_X.unique())
+print("=======================")
+print(datos_y.unique())
+
+X_train, X_test, y_train, y_test = train_test_split(
+    datos_X,
+    datos_y,
+    test_size=0.80,
+    random_state=42
+)
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Crear una instancia del vectorizador
+vectorizer = CountVectorizer()
+
+# Ajustar el vectorizador con los datos de entrenamiento
+Xtrain = vectorizer.fit_transform(X_train)
+
+# Transformar los datos de prueba utilizando el vectorizador ajustado
+Xtest = vectorizer.transform(X_test)
+
+print("=======================")
+print(Xtrain.shape)
+
+tokens = vectorizer.get_feature_names_out()
+print("=======================")
+print("Tokens:", tokens)
+print("Tokens:", len(tokens))
+
+print("=======================")
+print("Conjunto de entrenamiento - Características:", Xtrain.shape)
+print("Conjunto de entrenamiento - Etiquetas:", y_train.shape)
+print("Conjunto de prueba - Características:", Xtest.shape)
+print("Conjunto de prueba - Etiquetas:", y_test.shape)
 
 
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import RFE
+from sklearn.datasets import make_classification
+
+def cuasiConstantes(Xtrain, Xtest, threshold=0.02):
+    # Crear el objeto VarianceThreshold
+    selector = VarianceThreshold(threshold=threshold)
+
+    # Aplicar la eliminación de características cuasi-constantes en la matriz tf-idf de entrenamiento
+    xtrain = selector.fit_transform(Xtrain)
+
+    # Obtener los índices de las características seleccionadas
+    selected_feature_indices = selector.get_support(indices=True)
+
+    # Filtrar la matriz tf-idf de prueba utilizando los índices de características seleccionadas
+    xtest = selector.transform(Xtest)
+
+    num_selected_features = xtrain.shape[1]
+    print("=======================")
+    print("Número de características seleccionadas:", num_selected_features)
+    print("=======================")
+    return xtrain, xtest, selected_feature_indices
 
 
+def ScaracteristicasRandom(xtrain, y_train, n_estimators=100, max_depth=None, min_samples_split=2):
+    # Crear un estimador Random Forest con parámetros ajustados
+    rf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, min_samples_split=min_samples_split, n_jobs=-1)
+
+    # Crear un selector RFE con Random Forest y ranking
+    rfe = RFE(estimator=rf, step=2)
+
+    # Aplicar RFE al conjunto de datos
+    xtrainE = rfe.fit_transform(xtrain, y_train)
+    # Obtener las características seleccionadas
+    selected_features = rfe.support_
+
+    # Obtener el ranking de importancia de características
+    feature_ranking = rfe.ranking_
+
+    # Imprimir las características seleccionadas
+    print("Características seleccionadas:")
+    for i, feature in enumerate(selected_features):
+        if feature:
+            print(f"Feature {i+1}")
+
+    # Imprimir el ranking de características
+    print("Ranking de características :")
+    for i, rank in enumerate(feature_ranking):
+        print(f"Feature {i+1}: {rank}")
+        
 
 
+    return xtrainE, selected_features, feature_ranking
+
+from sklearn.feature_selection import VarianceThreshold
+xtrain, xtest, selected_feature_indices=cuasiConstantes(Xtrain, Xtest)
+#num_selected_features = xtrain.shape[1]
+
+xtrainE, selected_features, feature_ranking = ScaracteristicasRandom(xtrain, y_train)
+xtestE = xtest[:, selected_features]  # Conjunto de prueba transformado
 
 
+# Contar características seleccionadas
+num_selected_features = sum(selected_features)
+print("=======================")
+print("Número de características seleccionadas:", num_selected_features)
+
+# Crear el modelo de Random Forest
+modelo_rf = RandomForestClassifier(n_estimators=500)
+print("=======================")
+print("Dimensiones entreamiento :", xtrain.shape)
+print("Dimensiones prueba ", y_train.shape)
+
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+
+rf_Model = RandomForestClassifier()
+cross_val_scores = cross_val_score(rf_Model, Xtrain, y_train, cv=2)
+rf_Model.fit(Xtrain, y_train)
+# Evaluar el rendimiento del modelo en los datos de entrenamiento y prueba
+train_accuracy = rf_Model.score(Xtrain, y_train) * 100
+test_accuracy = rf_Model.score(Xtest, y_test) * 100
+print("=======================")
+print("Precisión del entrenamiento:", "{:.3f}%".format(train_accuracy))
+print("Precisión de prueba:", "{:.3f}%".format(test_accuracy))
+
+# Imprimir los resultados de la validación cruzada
+print("=======================")
+print("Resultados de la validación cruzada:")
+print(cross_val_scores)
+print("Precisión media: {:.2f}".format(cross_val_scores.mean()))
+
+# Crear el modelo de Random Forest
+modelo_rf = RandomForestClassifier(n_estimators=100, random_state=42)
+
+print("Dimensiones entreamiento :", xtrain.shape)
+print("Dimensiones prueba ", y_train.shape)
+
+# Número de árboles en el bosque aleatorio
+n_estimators = [int(x) for x in np.linspace(start=150, stop=400, num=4)]
+# Número de características a considerar en cada división
+max_features = ['auto', 'sqrt']
+# Número máximo de niveles en el árbol
+max_depth = None,
+# Número mínimo de muestras requeridas para dividir un nodo
+min_samples_split = [5, 10]
+# Número mínimo de muestras requeridas en cada nodo hoja
+min_samples_leaf = [1, 2]
+# Método de selección de muestras para entrenar cada árbol
+
+param_grid = {'n_estimators': n_estimators,
+               'max_features': max_features,
+               'max_depth': max_depth,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf,              
+                }
+print(param_grid)
+
+rf_Grid = GridSearchCV(estimator = rf_Model, param_grid = param_grid, cv = 2, verbose=2, n_jobs = 4)
+
+rf_Grid.fit(xtrain, y_train)
+
+rf_Grid.best_params_
+
+# Evaluar el rendimiento del modelo en los datos de entrenamiento y prueba
+train_accuracy = rf_Grid.score(xtrain, y_train) * 100
+test_accuracy = rf_Grid.score(xtest, y_test) * 100
+
+print("Precisión del entrenamiento:", "{:.3f}%".format(train_accuracy))
+print("Precisión de prueba:", "{:.3f}%".format(test_accuracy))
 
 
 
