ae1eff9e7df00eaf27a5f6b0d8310e428cce46fa
JoseQuinteroTC <67284004+JoseQuinteroTC@users.noreply.github.com>
2023-11-06 23:08:56 -0500

    arboles

--- IA ML/datos.py
+++ IA ML/datos.py
@@ -514,6 +514,78 @@ test_accuracy = rf_Grid.score(xtest, y_test) * 100
 print("Precisión del entrenamiento:", "{:.3f}%".format(train_accuracy))
 print("Precisión de prueba:", "{:.3f}%".format(test_accuracy))
 
+# Contar características seleccionadas
+num_selected_features = sum(selected_features)
+print("Número de características seleccionadas:", num_selected_features)
+
+# Crear el modelo de Random Forest
+modelo_rf = RandomForestClassifier(n_estimators=100, random_state=42)
+
+print("Dimensiones :", xtrainE.shape)
+print("Dimensiones ", y_train.shape)
+
+# Número de árboles en el bosque aleatorio
+n_estimators = [int(x) for x in np.linspace(start=100, stop=150, num=3)]
+#Número de características a considerar en cada división
+max_features = ['auto', 'sqrt']
+# Número máximo de niveles en el árbol
+max_depth = [2, 4]
+# Número mínimo de muestras requeridas para dividir un nodo
+min_samples_split = [2, 5]
+# Número mínimo de muestras requeridas en cada nodo hoja
+min_samples_leaf = [1, 2]
+
+param_grid = {
+    'n_estimators': n_estimators,
+    'max_features': max_features,
+    'max_depth': max_depth,
+    'min_samples_split': min_samples_split,
+    'min_samples_leaf': min_samples_leaf,
+}
+
+print(param_grid)
+
+rf_Grid = GridSearchCV(estimator = rf_Model, param_grid = param_grid, cv = 2, verbose=2, n_jobs = 4)
+
+rf_Grid.fit(xtrainE, y_train)
+
+
+
+# Evaluar el rendimiento del modelo en los datos de entrenamiento y prueba
+train_accuracy = rf_Grid.score(xtrainE, y_train) * 100
+test_accuracy = rf_Grid.score(xtestE, y_test) * 100
+
+print("Precisión del entrenamiento:", "{:.3f}%".format(train_accuracy))
+print("Precisión de prueba:", "{:.3f}%".format(test_accuracy))
+
+
+# Ajuste inicial del modelo
+dt = DecisionTreeClassifier()
+dt.fit(Xtrain, y_train)
+y_pred_initial = dt.predict(Xtest)
+
+# Matriz de confusión inicial
+dt_initial = confusion_matrix(y_test, y_pred_initial)
+dt_df_initial = pd.DataFrame(dt_initial)
+
+# Precisión inicial
+from sklearn.metrics import accuracy_score
+
+dt_precision_initial = 100 * accuracy_score(y_test, y_pred_initial)
+print('Precisión inicial: {0:.3f}%'.format(dt_precision_initial))
+
+from sklearn.datasets import make_classification
+from sklearn.metrics import classification_report
+
+# Generate a synthetic classification dataset
+
+y_pred_train = dt.predict(Xtrain)
+y_pred_test  = dt.predict(Xtest)
+
+print(classification_report(y_train, y_pred_train))
+print('----------------------------------------------------------')
+print(classification_report(y_test, y_pred_test))
+
 
 
 
